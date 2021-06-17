cf1aac9aeaadeb8048eaa0a19c0555c0eb9ce825
Nando Machado <icenando@gmail.com>
2021-06-17 14:54:23 +0100

    Link to character images extracted

--- app.py
+++ app.py
@@ -3,37 +3,41 @@
 # and output desired data to be rendered by templates html pages.
 
 import requests
-from config import MARVEL_URL
+from config import MARVEL_URL, generate_hash, app, API_PUBLIC
 from pprint import pprint
-
-from flask import Flask, render_template
-
-app = Flask(__name__)
+from flask import render_template
 
 
-def get_story() -> dict:
-    result = requests.get(MARVEL_URL)
+
+def get_url_info(url: str) -> dict:
+    result = requests.get(url)
     result.raise_for_status()
     result = (result.json())
-    return [
-        result['attributionText'], 
-        result['data']['results'][1]
-    ]
+    return result
+
+
+def char_img(hero_id: str) -> str:
+    timestamp, hash = generate_hash()
+    char_url = f"http://gateway.marvel.com/v1/public/characters/{hero_id}?ts={timestamp}&apikey={API_PUBLIC}&hash={hash}"
+    char_info = get_url_info(char_url)['data']['results'][0]['thumbnail']
+    img_url = '/'.join([char_info['path'], "standard_xlarge"]) + '.' + char_info['extension']
+    return img_url
 
 
 def get_characters(story: dict) -> dict:
-    characters_dict = {}
-    
+    chars_dict = {}
     for hero in story['characters']['items']:
-        characters_dict[hero['name']] = hero['resourceURI']
+        chars_dict[hero['name']] = char_img(hero['resourceURI'].split('/')[-1])
     
-    return characters_dict
+    return chars_dict
 
 
 @app.route('/', methods = ['GET'])
 def home() -> None:
     #The Marvel attribution text
-    attribution, story = get_story()
+    all_data = get_url_info(MARVEL_URL)
+    attribution = all_data['attributionText']
+    story = all_data['data']['results'][1]
     
     #The story's description
     description = story['description']
@@ -42,10 +46,16 @@ def home() -> None:
     # of the characters that features in the story
     characters = get_characters(story)
 
-
-    return render_template('home.html', description=description, characters=characters, attribution=attribution)
+    #Characters pictures
+    return render_template(
+        'home.html', 
+        description=description, 
+        characters=characters, 
+        attribution=attribution
+    )
 
 
 
 if __name__ == "__main__":
     app.run()
+    # home()
--- config.py
+++ config.py
@@ -4,16 +4,22 @@
 import os
 import time
 import hashlib
+from flask import Flask
 
+app = Flask(__name__)
 
-CHARACTER_ID = "1009362" #iceman
+def generate_hash():
+    TIMESTAMP = str(time.time()).replace(".",'')
+    # Marvel's API server-side requests hash format requirement: digested md5 hash of "timestamp + private key + public key")
+    HASH = hashlib.md5((TIMESTAMP+API_PRIVATE+API_PUBLIC).encode()).hexdigest()
+    return TIMESTAMP, HASH
+
+CHARACTER_ID = "1009362"  #iceman
 COMIC_ID = "13881"
 
-TIMESTAMP = str(time.time()).replace(".",'')
 API_PRIVATE = os.environ["API_PRIVATE"]
 API_PUBLIC = os.environ["API_PUBLIC"]
 
-# Marvel's API server-side requests hash format requirement: digested md5 hash of "timestamp + private key + public key")
-HASH = hashlib.md5((TIMESTAMP+API_PRIVATE+API_PUBLIC).encode()).hexdigest()
-
+TIMESTAMP, HASH = generate_hash()
 MARVEL_URL = f"https://gateway.marvel.com:443/v1/public/comics/{COMIC_ID}/stories?ts={TIMESTAMP}&apikey={API_PUBLIC}&hash={HASH}"
+print(MARVEL_URL)
