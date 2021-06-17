1ed5635bb74c8bd89355b0689517c6cd7139255b
Nando Machado <icenando@gmail.com>
2021-06-17 13:10:20 +0100

    Implemented basic home.html

--- app.py
+++ app.py
@@ -6,13 +6,46 @@ import requests
 from config import MARVEL_URL
 from pprint import pprint
 
+from flask import Flask, render_template
 
-def api_request() -> requests:
+app = Flask(__name__)
+
+
+def get_story() -> dict:
     result = requests.get(MARVEL_URL)
     result.raise_for_status()
+    result = (result.json())
+    return [
+        result['attributionText'], 
+        result['data']['results'][1]
+    ]
 
-    result = result.json()
-    pprint(result)
+
+def get_characters(story: dict) -> dict:
+    characters_dict = {}
+    
+    for hero in story['characters']['items']:
+        characters_dict[hero['name']] = hero['resourceURI']
+    
+    return characters_dict
+
+
+@app.route('/', methods = ['GET'])
+def home() -> None:
+    #The Marvel attribution text
+    attribution, story = get_story()
+    
+    #The story's description
+    description = story['description']
+    
+    #A list of names and pictures 
+    # of the characters that features in the story
+    characters = get_characters(story)
+
+
+    return render_template('home.html', description=description, characters=characters, attribution=attribution)
+
+
 
 if __name__ == "__main__":
-    api_request()
+    app.run()
--- config.py
+++ config.py
@@ -1,20 +1,19 @@
 #! python3
-
+# Config variables for app.py.
 
 import os
-from flask import Flask
 import time
 import hashlib
 
-app = Flask(__name__)
+
+CHARACTER_ID = "1009362" #iceman
+COMIC_ID = "13881"
 
 TIMESTAMP = str(time.time()).replace(".",'')
 API_PRIVATE = os.environ["API_PRIVATE"]
 API_PUBLIC = os.environ["API_PUBLIC"]
 
-# Marvel's API server-side requests hash format requirement: 
-# digested md5 hash of "timestamp + private key + public key")
+# Marvel's API server-side requests hash format requirement: digested md5 hash of "timestamp + private key + public key")
 HASH = hashlib.md5((TIMESTAMP+API_PRIVATE+API_PUBLIC).encode()).hexdigest()
 
-MARVEL_URL = f"https://gateway.marvel.com:443/v1/public/stories?ts={TIMESTAMP}&apikey={API_PUBLIC}&hash={HASH}"
-# print(MARVEL_URL)
+MARVEL_URL = f"https://gateway.marvel.com:443/v1/public/comics/{COMIC_ID}/stories?ts={TIMESTAMP}&apikey={API_PUBLIC}&hash={HASH}"
